bd06149c54341d2c571f3a7c11a434efd32e9346
Tatiana Dara de Aragao Santos <dara.aragao17@gmail.com>
2025-07-18 18:49:37 -0300

    feat: subnet privada e variables

--- terraform_aws/variables.tf
+++ terraform_aws/variables.tf
@@ -2,3 +2,8 @@ variable "cidr_block" {
   type        = string
   description = "Cidr block VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Projeto usado para nome dos recrusos (Name tag)"
+}
